6b96fe3f91b210a56c607310ee6c1e33ab7f657b
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-05-17 23:43:12 +0200

    add link to logo for home redirection

--- app/components/atoms/logo.tsx
+++ app/components/atoms/logo.tsx
@@ -1,15 +1,19 @@
+'use client'
 import Image from 'next/image'
+import Link from 'next/link'
 import logoAF from '@/assets/logoAF.jpeg'
 
-const logo = () => {
+const Logo = () => {
   return (
-    <Image
-      src={logoAF}
-      alt="Logo d'Anne-Flore Bernard"
-      className="h-[114px] w-[300px] object-contain absolute top-8 right-[204px]"
-      aria-hidden="true"
-    />
+    <Link href="/" aria-label="Aller à la page d'accueil">
+      <Image
+        src={logoAF}
+        alt="Logo d'Anne-Flore Bernard"
+        className="h-[114px] w-[300px] object-contain absolute top-8 right-[204px]"
+        aria-hidden="true"
+      />
+    </Link>
   )
 }
 
-export default logo
+export default Logo
